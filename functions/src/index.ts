26c8bd7e1b5db5c77228499a58178d4ec681b380
jjegarcia <jjegarcia@hotmail.com>
2024-07-07 10:05:56 +0100

    first commit

---
+++ functions/src/index.ts
@@ -0,0 +1,46 @@
+import {generate} from "@genkit-ai/ai";
+import {configureGenkit} from "@genkit-ai/core";
+import {firebaseAuth} from "@genkit-ai/firebase/auth";
+import {onFlow} from "@genkit-ai/firebase/functions";
+
+import * as z from "zod";
+import {firebase} from "@genkit-ai/firebase";
+
+configureGenkit({
+  plugins: [
+    firebase(),
+  ],
+  logLevel: "debug",
+  enableTracingAndMetrics: true,
+});
+
+export const menuSuggestionFlow = onFlow(
+  {
+    name: "menuSuggestionFlow",
+    inputSchema: z.string(),
+    outputSchema: z.string(),
+    authPolicy: firebaseAuth((user) => {
+      // Firebase Auth is required to call this flow using the Firebase Functions SDK.
+      // TODO: Write additional logic tailored to the needs of your app.
+      // For example:
+      // if (!user.email_verified) {
+      //   throw new Error("Verified email required to run flow");
+      // }
+    }),
+  },
+  async (subject) => {
+    const prompt =
+      `Suggest an item for the menu of a ${subject} themed restaurant`;
+
+    const llmResponse = await generate({
+      model: '' /* TODO: Set a model. */,
+      prompt: prompt,
+      config: {
+        temperature: 1,
+      },
+    });
+
+    return llmResponse.text();
+  }
+);
+
